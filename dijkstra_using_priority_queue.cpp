51c4a40fd4934747c7280db3cdab569fcaca7b96
Sayak <44060681+SAYAKGHANTA@users.noreply.github.com>
2021-06-15 00:45:34 +0530

    Update dijkstra_using_priority_queue.cpp

--- dijkstra_using_priority_queue.cpp
+++ dijkstra_using_priority_queue.cpp
@@ -1,25 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<vector<pair<int,int>>>adj;
-priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;//min-heap priority queue
 
 int main()
 {
     int v,e;
     cin>>v>>e;
-    adj.resize(v);
+    vector<vector<pair<int,int>>>adj(V);
     for(int i=0;i<e;i++)
     {
         int u,v,w;
         cin>>u>>v>>w;
         adj[u].push_back({v,w});
-        adj[v].push_back({u,w});
+        adj[v].push_back({u,w});// comment out this for directed graph
     }
+    
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;//min-heap priority queue
+    
     int source;
     cin>>source;
+    
     pq.push({source,0});//priority queue =>{node,distance}
+    
     vector<int>dist(v,INT_MAX);//distance array to store distance
     dist[source]=0;
+    
     while(!pq.empty())
     {
         auto p=pq.top();
